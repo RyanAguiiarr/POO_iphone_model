1eeaefbed83133058d4054ae58676c9dc41468b2
ryan <ryan.c.aguiiarr@gmail.com>
2025-03-15 01:45:49 -0300

    update

---
+++ iphone.java
@@ -0,0 +1,88 @@
+// Definição das interfaces
+
+interface ReprodutorMusical {
+    void tocar();
+    void pausar();
+    void selecionarMusica(String musica);
+}
+
+interface AparelhoTelefonico {
+    void ligar(String numero);
+    void atender();
+    void iniciarCorreioVoz();
+}
+
+interface NavegadorInternet {
+    void exibirPagina(String url);
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
+
+// Implementação da classe iPhone que herda as funcionalidades das interfaces
+class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    // Métodos do Reprodutor Musical
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    // Métodos do Aparelho Telefônico
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    // Métodos do Navegador na Internet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    public static void main(String[] args) {
+        iPhone meuIphone = new iPhone();
+
+        // Testando funcionalidades
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+        meuIphone.tocar();
+        meuIphone.pausar();
+
+        meuIphone.ligar("+55 11 99999-9999");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        meuIphone.exibirPagina("https://www.apple.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
